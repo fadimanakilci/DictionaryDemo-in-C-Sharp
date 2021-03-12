c8e966ed322b53afd698e453d95f044b70337dc7
Fadimana Kilci <fadimekilci07@gmail.com>
2021-03-13 00:17:01 +0300

    Initial Commit

---
+++ MyDictionary.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DictionaryDemo
+{
+    class MyDictionary<T1, T2>
+    {
+        T1[] desserts;
+        T2[] prices;
+        public MyDictionary()
+        {
+            desserts = new T1[0];
+            prices = new T2[0];
+        }
+        public void Add(T1 dessert, T2 price)
+        {
+            T1[] tempArrayd = desserts;
+            T2[] tempArrayp = prices;
+            desserts = new T1[desserts.Length + 1];
+            prices = new T2[prices.Length + 1];
+            for (int i = 0; i < tempArrayp.Length; i++)
+            {
+                desserts[i] = tempArrayd[i];
+                prices[i] = tempArrayp[i];
+
+            }
+            desserts[desserts.Length - 1] = dessert;
+            prices[prices.Length - 1] = price;
+        }
+        public void List()
+        {
+            for (int i = 0; i < desserts.Length; i++)
+            {
+                Console.WriteLine(desserts[i] + "\t   ->\t" + prices[i]);
+            }
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace DictionaryDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n  ~~~~ Welcome to our bakery ~~~~\n");
+            MyDictionary<string, float> myDictionary = new MyDictionary<string, float>();
+            Console.WriteLine("Please enter the number of desserts to add");
+            int numb = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("___________________________________________________\n");
+            for (int i = 1; i <= numb; i++)
+            {
+                Console.WriteLine("[" + i + "]  Please enter the dessert first, then the price");
+                myDictionary.Add(Console.ReadLine(), float.Parse(Console.ReadLine()) );
+            }
+            Console.WriteLine("\n___________________________________________________\n");
+            myDictionary.List();
+        }
+    }
+}
